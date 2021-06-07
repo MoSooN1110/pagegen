22c5af97d0b77d79e5d8f7e3fc7117714d31f752
MoSooN <azurite1110@gmail.com>
2021-06-07 21:04:32 +0900

    up to latest page

--- src/page_gen_past.py
+++ src/page_gen_past.py
@@ -30,7 +30,7 @@ def template_proc(file_str):
 
 
 def doc_proc(file_str):
-    file_name_html = file_str+"_data.html"
+    file_name_html = file_str+"_main.html"
     # ファイルを読み込む
     with open(file_name_html,"r",encoding="utf-8") as file:
         filedata=file.read()
@@ -67,50 +67,18 @@ def doc_pre_proc(file_str):
 
     return
 
-
-def data_mege(file_str):
-    file_name_html2 = file_str+"_data.html"
-    file_name_html1 = file_str+".html"
-    # ファイルを読み込む
-    
-    with open(file_name_html1,"r",encoding="utf-8") as file1:
-        filedata1=file1.read()
-        filedata = ''
-        with open(file_name_html2,"r",encoding="utf-8") as file2:
-            filedata2=file2.read()
-            idx = filedata1.find("<!-- #ID0001:article -->")
-            filedata += filedata1[:idx]
-            filedata += filedata2
-            filedata += filedata1[idx:]
-            # with open(file_name_html1,"w",encoding="utf-8") as file:
-            #     file.write(filedata1[:idx])
-            # with open(file_name_html1,"a",encoding="utf-8") as file:
-            #     file.write(filedata2)
-            # with open(file_name_html1,"a",encoding="utf-8") as file:
-            #     file.write(filedata1[idx:])
-            
-    # ファイルに書き込む
-    print(filedata)
-    with open(file_name_html1,"w",encoding="utf-8") as file:
-        file.write(filedata)
-
-    return
 def main_proc():
     file_name = sys.argv[1]
     file_str = file_name[:-3]
-    if len(sys.argv) == 1:
-        print("uasge >> python page_gen.py input.md template.html")
-        return
 
     print("process >> "+file_str)
     doc_pre_proc(file_str)
     template_file_path = sys.argv[2]
-    subprocess.call(["pandoc", "tmp.md", "-o", file_str+"_data.html"])
+    subprocess.call(["pandoc", "tmp.md", "-o", file_str+"_main.html"])
     subprocess.call(["cp", template_file_path , file_str+".html"])
     
     template_proc(file_str)
     doc_proc(file_str)
-    data_mege(file_str)
     
     subprocess.call(["mv",file_str+"_main.html","output/"+file_str+"_main.html"])
     subprocess.call(["mv",file_str+".html","output/"+file_str+".html"])
